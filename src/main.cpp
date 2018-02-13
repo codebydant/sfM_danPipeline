757b3209ae8c47d3eb9327dbc7bc62d5d93afaa4
Daniel <35694200+danielTobon43@users.noreply.github.com>
2018-02-12 21:30:44 -0500

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -15,55 +15,45 @@
 
 int main(int argc, char **argv ){
 
-   cv::Mat image1,image2;
-   cv::Mat temp_img2;
+   cv::Mat image1,image2,temp_img2;
    std::string frame1,frame2;
    std::ifstream file("temple/list.txt");
 
 std::cout << "************************************************" << std::endl;
 std::cout << "************************************************" << std::endl;
 
-   std::getline(file, frame1);
-   std::cout << frame1 << std::endl;
-   std::getline(file, frame2);
-   std::cout << frame2<< std::endl;
-   std::cout << "----------------------------------" << std::endl;
-   std::cout << "----------------------------------" << std::endl;
+  std::cout << "----------------------------------" << std::endl;
+  std::getline(file, frame1);
+  std::cout << frame1 << std::endl;
+  std::getline(file, frame2);
+  std::cout << frame2<< std::endl;
+  std::cout << "----------------------------------" << std::endl;
 
-  image1 = cv::imread(frame1,0);
-  image2 = cv::imread(frame2,0);
-
-  GaussianBlur(image1,image1, cv::Size(7,7),1.5,1.5);
-  GaussianBlur(image2,image2, cv::Size(7,7),1.5,1.5);
+  image1 = cv::imread(frame1,CV_LOAD_IMAGE_COLOR);
+  image2 = cv::imread(frame2,CV_LOAD_IMAGE_COLOR);
 
   StructFromMotion sfm(image1,image2);
   sfm.matchingImShow();
   sfm.initTriangulation();
-
   temp_img2 = image2;
 
-  for(int n=1;n<5;n++){
+  for(int n=1;n<18;n++){
 
         image1=temp_img2;
-
-        std::getline(file, frame2);
         std::cout << "----------------------------------" << std::endl;
+        std::getline(file, frame2);        
         std::cout << frame2 <<std::endl;
-        image2 = cv::imread(frame2,0);
+        image2 = cv::imread(frame2,CV_LOAD_IMAGE_COLOR);
         std::cout << "----------------------------------" << std::endl;
-
-        GaussianBlur(image1,image1, cv::Size(7,7),1.5,1.5);
-        GaussianBlur(image2,image2, cv::Size(7,7),1.5,1.5);
 
         sfm.setConstructor(temp_img2,image2);
-
         temp_img2 = image2;
         sfm.matchingImShow();
         sfm.initTriangulation();
 
         }
-  sfm.visualizerPointCloud();
 
+  sfm.visualizerPointCloud();
 
 std::cout << "************************************************" << std::endl;
 std::cout << "************************************************" << std::endl;
