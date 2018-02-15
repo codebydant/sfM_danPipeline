634b10c06ffe5086c1ce5c42461d7fb96a1569fe
Daniel <35694200+danielTobon43@users.noreply.github.com>
2018-02-15 10:06:16 -0500

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include "../include/Sfm.h"
 #include "../include/Bundler_adjustment.h"
 #include "../include/Ros_interface.h"
+//#include "Sfm.cpp"
 
 /*********************************
       FUNCION PRINCIPAL-MAIN
@@ -32,12 +33,15 @@ std::cout << "************************************************" << std::endl;
   image1 = cv::imread(frame1,CV_LOAD_IMAGE_COLOR);
   image2 = cv::imread(frame2,CV_LOAD_IMAGE_COLOR);
 
-  StructFromMotion sfm(image1,image2);
-  sfm.matchingImShow();
+   StructFromMotion sf;
+
+
+  std::vector<cv::Point3d> cloud =sf.recon(image1,image2);
+
   //sfm.initTriangulation();
   temp_img2 = image2;
-
-  for(int n=1;n<15;n++){
+/*
+  for(int n=1;n<3;n++){
 
         image1=temp_img2;
         std::cout << "----------------------------------" << std::endl;
@@ -53,7 +57,21 @@ std::cout << "************************************************" << std::endl;
 
         }
 
-  sfm.visualizerPointCloud();
+*/
+
+/*
+              for (int i = 0; i < bal_problem.num_observations(); ++i) {
+                ceres::CostFunction* cost_function =
+                    SnavelyReprojectionError::Create(
+                         bal_problem.observations()[2 * i + 0],
+                         bal_problem.observations()[2 * i + 1]);
+                problem.AddResidualBlock(cost_function,
+                                         NULL ,
+                                         bal_problem.mutable_camera_for_observation(i),
+                                         bal_problem.mutable_point_for_observation(i));
+              }
+*/
+
 
 std::cout << "************************************************" << std::endl;
 std::cout << "************************************************" << std::endl;
