2f40424178099bea3330baf463f401fe539e5c0b
Daniel <35694200+danielTobon43@users.noreply.github.com>
2018-02-19 10:11:50 -0500

    Add files via upload

--- include/Sfm.h
+++ include/Sfm.h
@@ -1,6 +1,7 @@
-//----------------------------------------------
+//***********************************************
 //HEADERS
-//----------------------------------------------
+//***********************************************
+
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -13,87 +14,122 @@
 #include "opencv2/viz/vizcore.hpp"
 #include "opencv2/viz/viz3d.hpp"
 
+//***********************************************
+//ALIAS
+//***********************************************
+
 using Keypoints = std::vector<cv::KeyPoint>;
 using MatchesVector = std::vector<cv::DMatch>;
 using Points2f = std::vector<cv::Point2f>;
+using Points3f = std::vector<cv::Point3f>;
+using Points3d = std::vector<cv::Point3d>;
 
 class StructFromMotion{ 
+
+  //===============================================
+  //ATRIBUTOS
+  //===============================================
 
   cv::Mat_<cv::Vec3b> image1, image2;
 
   public:
 
-  //----------------------------------------------
+  //===============================================
   //CONSTRUCTOR
-  //----------------------------------------------
+  //===============================================
   StructFromMotion(){}
   StructFromMotion(cv::Mat& img1,cv::Mat& img2);
 
-  //----------------------------------------------
-  //PIPELINE FUNCTIONS
-  //----------------------------------------------
+  //===============================================
+  //CARGA DE IMAGENES
+  //===============================================
 
   void recon( std::ifstream& file);
-
   int sizeTxtFile( std::ifstream& file);
+
+  //===============================================
+  //FEATURE DETECTION AND EXTRACTION
+  //===============================================
 
   Keypoints obtenerKeypoints (cv::Mat& image);
   cv::Mat obtenerDescriptors (cv::Mat& image,Keypoints& keypoints);
 
+  //===============================================
+  //FEATURE MATCHING
+  //===============================================
+
   MatchesVector obtenerMatches(cv::Mat& descriptors1,cv::Mat& descriptors2);
-
-  cv::Mat imageMatching(cv::Mat& img1, Keypoints& keypoints1,cv::Mat& img2, Keypoints& keypoints2,
-                        MatchesVector& matches);
-
+  cv::Mat imageMatching(cv::Mat& img1, Keypoints& keypoints1,
+                        cv::Mat& img2, Keypoints& keypoints2,MatchesVector& matches);
   void matchingImShow(cv::Mat& matchImage);
+
+  //===============================================
+  //CONVERTION KEYPOINTS TO POINTS2F
+  //===============================================
 
   Points2f keypoints2F(Keypoints& keypoints,MatchesVector& matches);
 
+  //===============================================
+  //CAMERA MATRIX
+  //===============================================
+
   cv::Mat_<double> getCameraMatrix();
 
- cv::Mat_<double> findEssentialMatrix( Points2f& leftPoints,Points2f& rightPoints,cv::Mat_<double>& cameraMatrix,cv::Mat& mask);
+  //===============================================
+  //ESSENTIAL MATRIX
+  //===============================================
 
-  void cameraPose(Points2f& points1,Points2f& points2,
-                  double& fx,double cx,double cy,cv::Mat& rot,cv::Mat& tra,
-                  cv::Mat& inliers,cv::Mat_<double>& essentialMatrix );
+  cv::Mat_<double> findEssentialMatrix(Points2f& leftPoints,Points2f& rightPoints,
+                                       cv::Mat_<double>& cameraMatrix,cv::Mat& mask);
+
+  //===============================================
+  //ROTATION AND TRASLATION MATRIX[R|t]
+  //===============================================
+
+  void cameraPose(Points2f& points1,Points2f& points2,double& fx,double cx,double cy,
+                  cv::Mat& rot,cv::Mat& tra,cv::Mat& inliers,cv::Mat_<double>& essentialMatrix );
+
+  //===============================================
+  //PROJECTION MATRIX
+  //===============================================
 
   void projection(const cv::Mat& relativeRotationCam,const cv::Mat& relativeTranslaCam,
-                  cv::Mat_<double>& projection1, cv::Mat_<double>& projection2);
+                  cv::Mat& projection1, cv::Mat& projection2);
 
- // std::vector<cv::Point3d> triangulation(std::vector<cv::Point2f>& points1,
-                                     //    std::vector<cv::Point2f>& points2,
-                                     //    cv::Mat_<double>& projection1,
-                                     //    cv::Mat_<double>& projection2,cv::Mat& inliers,cv::Mat_<double>& cameraMatrix);
 
+  //===============================================
+  //FUNCTION CHECK ROTATION MATRIX (Must be det=1)
+  //===============================================
+
+  bool CheckCoherentRotation(cv::Mat& R);
+
+  //===============================================
+  //FUNCTION ALIGNED POINTS
+  //===============================================
+
+  void AlignedPointsFromMatch(Keypoints& left,Keypoints& right, MatchesVector& matches,
+                              Points2f& featuresLeftAligned,Points2f& featuresRightAligned);
+
+  //===============================================
+  //FUNCTION CORRESPONDENCES 2D-3D
+  //===============================================
+
+
+
+  //===============================================
+  //POINTCLOUD VISUALIZER
+  //===============================================
   void visualizerPointCloud(cv::Matx33d& cameraMatrix,cv::Mat& img1,
-                            cv::Mat& img2,cv::Mat& cameraR,cv::Mat& cameraT,cv::Mat& pointcloud);
+                            cv::Mat& img2,cv::Mat& cameraR,cv::Mat& cameraT,Points3d& pointcloud);
 
-  void setConstructor(cv::Mat& img1,cv::Mat& img2);
-
-  //----------------------------------------------
-  //INVERSE MATRIX FUNCTION EIGEN
-  //----------------------------------------------
+  //===============================================
+  //INVERSE MATRIX-DETERMINANT FUNCTION EIGEN
+  //===============================================
 
   cv::Mat inverse(cv::Mat& matrix);
-
   double determinante(cv::Mat& relativeRotationCam);
 
-  //----------------------------------------------
-  //FUNCTION CHECK ROTATION MATRIX (Must be det=1)
-  //----------------------------------------------
-
-  bool CheckCoherentRotation(cv::Mat_<double>& R); 
-
-  //----------------------------------------------
-  //FUNCTION ALIGNED POINTS
-  //----------------------------------------------
-
-  void AlignedPointsFromMatch(Keypoints& left,Keypoints& right, MatchesVector& matches, Points2f& featuresLeftAligned,Points2f& featuresRightAligned);
-
-  void Find2D3DCorrespondences(int working_view,std::vector<cv::Point3f>& ppcloud,
-          Points2f& imgPoints);
-
-
+  Points3d cloudPointsCoordinates(const Points3d cloudpoint);
 
 
 };//Fin class
