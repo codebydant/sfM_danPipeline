f910cef1ebaa94fdc7ac8441117d250a100803da
Daniel <35694200+danielTobon43@users.noreply.github.com>
2018-03-11 21:28:54 -0500

    Add files via upload

--- include/Structures.h
+++ include/Structures.h
@@ -1,3 +1,7 @@
+//***********************************************
+//HEADERS
+//***********************************************
+
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/xfeatures2d.hpp>
