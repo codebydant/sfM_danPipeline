245c520cbfc64b1d3bf4a2b679150b9e60031006
Daniel <35694200+danielTobon43@users.noreply.github.com>
2018-03-11 21:28:33 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -11,16 +11,9 @@
 
 int main(int argc, char **argv ){
 
-
-  std::cout << "************************************************" << std::endl;
-  std::cout << "************************************************" << std::endl;
-
   std::ifstream file("temple/list.txt");
   StructFromMotion sf;
   int success = sf.run_SFM(file);
-
-  std::cout << "************************************************" << std::endl;
-  std::cout << "************************************************" << std::endl;
 
   return 0;
 
