c63079e3b2f594e28ff083bff26bbdae9ce1fdd6
Daniel <35694200+danielTobon43@users.noreply.github.com>
2018-04-20 10:41:59 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -11,20 +11,13 @@
 
 int main(int argc, char **argv ){
 
-
   //QApplication container(argc, argv);
   //Visualizer window;
 
   std::ifstream file("temple/list.txt");
   StructFromMotion sf; 
-  sf.multithreading(file);
+  sf.run_SFM(file);
 
-
-
-
-
-
-    return 0;
-
+  return 0;
 
 }//end main
