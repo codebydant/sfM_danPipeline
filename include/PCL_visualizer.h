66123cf648366b3026ecbb38564e23c8626681d8
Daniel <35694200+danielTobon43@users.noreply.github.com>
2018-04-30 21:31:08 -0500

    Delete PCL_visualizer.h

--- include/PCL_visualizer.h
+++
@@ -1,8 +0,0 @@
-#include <pcl/io/pcd_io.h>
-#include <pcl/point_cloud.h>
-#include <pcl/point_types.h>
-#include <pcl/visualization/pcl_visualizer.h>
-//#include <pcl/kdtree/kdtree_flann.h>
-//#include <pcl/features/normal_3d.h>
-//#include <pcl/surface/gp3.h>
-
