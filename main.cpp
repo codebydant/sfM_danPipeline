62bb1fee1143c352aeab4a49b3e47679fd9264bb
Daniel <35694200+danielTobon43@users.noreply.github.com>
2018-06-11 17:31:32 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -43,19 +43,13 @@ int main(int argc, char **argv){
        return 1;
    }*/
 
-  //Segmentation sg;
-  //sg.color_based_growing_segmentation();
-
-  Visualizer vs;
-//  vs.RunVisualizationThread();
-
   StructFromMotion sf;    
-  bool success = sf.imagesLOAD("../../data/temple");
+  bool success = sf.imagesLOAD("/home/daniel/Proyecto-grado-3D-recons-master/data/temple");
   if(not success){
       std::cerr << "Error: set of images is not valid." << std::endl;
       return -1;
   }
-  success = sf.getCameraMatrix("../../data/temple/camera_calibration_template.xml");
+  success = sf.getCameraMatrix("/home/daniel/Proyecto-grado-3D-recons-master/data/temple/camera_calibration_template.xml");
   if(not success){
       std::cerr << "Error: camera calibration file is not valid." << std::endl;
       return -1;
@@ -67,8 +61,32 @@ int main(int argc, char **argv){
       return -1;
   }
 
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloudPCL(new pcl::PointCloud<pcl::PointXYZRGB> ());
 
+  for(size_t i = 0; i < sf.nReconstructionCloud.size(); ++i){
+      Point3D pt3d =  sf.nReconstructionCloud[i];
+      cv::Vec3b rgbv(255,255,255);
+      pcl::PointXYZRGB pclp;
+      pclp.x  = pt3d.pt.x;
+      pclp.y  = pt3d.pt.y;
+      pclp.z  = pt3d.pt.z;
+      rgbv =  sf.nReconstructionCloudRGB[i];
+
+      // RGB color, needs to be represented as an integer
+      uint32_t rgb = ((uint32_t)rgbv[2] << 16 | (uint32_t)rgbv[1] << 8 | (uint32_t)rgbv[0]);
+      pclp.rgb = *reinterpret_cast<float*>(&rgb);
+      cloudPCL->push_back(pclp);
+   }
+
+   cloudPCL->width = (uint32_t) cloudPCL->points.size(); // number of points
+   cloudPCL->height = 1;	// a list, one row of data
+   cloudPCL->header.frame_id ="map3d";
+   cloudPCL->is_dense = false;
+
+
+  Segmentation sg;
+  sg.color_based_growing_segmentation(cloudPCL);
 
   return 0;
 
-}//end main
+}
