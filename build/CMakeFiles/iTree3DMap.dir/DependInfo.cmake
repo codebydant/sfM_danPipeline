d6923caf0ae80165869c64ea52f93b8bee1337dc
seb-chu <sebastianclc123@gmail.com>
2024-02-06 19:20:02 +0000

    updated /include/ files and got make to work

---
+++ build/CMakeFiles/iTree3DMap.dir/DependInfo.cmake
@@ -0,0 +1,51 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/workspaces/test-pipeline/SfM_pipeline/main.cpp" "/workspaces/test-pipeline/SfM_pipeline/build/CMakeFiles/iTree3DMap.dir/main.cpp.o"
+  "/workspaces/test-pipeline/SfM_pipeline/src/BundleAdjustment.cpp" "/workspaces/test-pipeline/SfM_pipeline/build/CMakeFiles/iTree3DMap.dir/src/BundleAdjustment.cpp.o"
+  "/workspaces/test-pipeline/SfM_pipeline/src/DendrometryE.cpp" "/workspaces/test-pipeline/SfM_pipeline/build/CMakeFiles/iTree3DMap.dir/src/DendrometryE.cpp.o"
+  "/workspaces/test-pipeline/SfM_pipeline/src/Segmentation.cpp" "/workspaces/test-pipeline/SfM_pipeline/build/CMakeFiles/iTree3DMap.dir/src/Segmentation.cpp.o"
+  "/workspaces/test-pipeline/SfM_pipeline/src/Sfm.cpp" "/workspaces/test-pipeline/SfM_pipeline/build/CMakeFiles/iTree3DMap.dir/src/Sfm.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DISABLE_LIBUSB_1_0"
+  "DISABLE_OPENNI"
+  "DISABLE_OPENNI2"
+  "DISABLE_PCAP"
+  "DISABLE_PNG"
+  "GFLAGS_IS_A_DLL=0"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_WIDGETS_LIB"
+  "qh_QHpointer"
+  "vtkRenderingContext2D_AUTOINIT=1(vtkRenderingContextOpenGL2)"
+  "vtkRenderingCore_AUTOINIT=3(vtkInteractionStyle,vtkRenderingFreeType,vtkRenderingOpenGL2)"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/vtk-7.1"
+  "/usr/include/freetype2"
+  "/usr/local/include/opencv4"
+  "/usr/include/pcl-1.10"
+  "/usr/include/eigen3"
+  "/usr/include/x86_64-linux-gnu/qt5"
+  "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
